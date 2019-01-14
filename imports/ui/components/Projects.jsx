b70e80313358c265f838a5c787f212a482d94369
Servio Mora <servio.mora19@gmail.com>
2019-01-14 15:23:30 -0500

    added projets to json, and fixed about overlay positioning

--- imports/ui/components/Projects.jsx
+++ imports/ui/components/Projects.jsx
@@ -7,19 +7,23 @@ export default class Projects extends Component {
 
   blocks() {
     return [
-      {"title": "Project 1"},
-      {"title": "Project 2"},
-      {"title": "Project 3"}
+      {"_id": 1, "name": "qc-site", "url": "http://quantifiedcommerce.com", "title": "Quantified Commerce", "description": "", "landing-img": ""},
+      {"_id": 2, "name": "9h-landing", "url": "", "title": "9H-Nano Coating", "description": "", "landing-img": ""},
+      {"_id": 3, "name": "sticky-pad", "url": "", "title": "MultiPurpose Gel Sticky Pad", "description": "", "landing-img": ""},
+      {"_id": 4, "name": "phone-mount", "url": "", "title": "Automatic Locking Phone Mount", "description": "", "landing-img": ""},
+      {"_id": 5, "name": "finance-app", "url": "", "title": "Finance App", "description": "", "landing-img": ""}
     ]
   }
 
   renderBlocks() {
     return this.blocks().map(block => {
       return (
-        <section key={block.title}>
-          <div className="block">
-            <h2>{block.title}</h2>
-          </div>
+        <section key={block._id}>
+          <a style={{textDecoration: 'none'}} href={block.url} target="_blank">
+            <div className="block">
+              <h2>{block.name}</h2>
+            </div>
+          </a>
         </section>
       )
     })
