a3f3dec6df6662158caf993252b27f1190715b3e
Servio Mora <servio.mora19@gmail.com>
2019-06-03 23:06:14 -0400

    started dynamic showing of projects by type state

--- imports/ui/components/Projects/Projects.jsx
+++ imports/ui/components/Projects/Projects.jsx
@@ -6,25 +6,45 @@ import { StyledProjects, Container, Section, Block } from './style.js'
 export default class Projects extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      projects: {
+        all: true,
+        ecommerce: false,
+        side_project: false
+      }
+    }
   }
 
   blocks() {
     return [
-      {"_id": 1, "name": "qc-site", "url": "http://quantifiedcommerce.com", "title": "Quantified Commerce", "description": "", "landing-img": ""},
-      {"_id": 2, "name": "9h-landing", "url": "", "title": "9H-Nano Coating", "description": "", "landing-img": ""},
-      {"_id": 3, "name": "sticky-pad", "url": "", "title": "MultiPurpose Gel Sticky Pad", "description": "", "landing-img": ""},
-      {"_id": 4, "name": "phone-mount", "url": "", "title": "Automatic Locking Phone Mount", "description": "", "landing-img": ""},
-      {"_id": 5, "name": "finance-app", "url": "", "title": "Finance App", "description": "", "landing-img": ""}
+      {"_id": 1, "name": "qc-site", "type": "front_end", "url": "http://quantifiedcommerce.com", "title": "Quantified Commerce Website", "description": "", "landing-img": ""},
+      {"_id": 2, "name": "9h-page", "type": "ecommerce", "url": "", "title": "9H-Nano Car Coating", "description": "Custom ecommerce webpage for our popular and great selling car polish", "landing-img": ""},
+      {"_id": 3, "name": "phone-mount", "type": "ecommerce", "url": "", "title": "Automatic Locking Phone Mount", "description": "", "landing-img": ""},
+      {"_id": 4, "name": "sticky-pad", "type": "ecommerce", "url": "", "title": "Best Gel Pad", "description": "", "landing-img": ""},
+      {"_id": 5, "name": "laundry-betty", "type": "ecommerce", "url": "", "title": "Laundry Betty", "description": "", "landing-img": ""},
+      {"_id": 6, "name": "fur-brush", "type": "", "url": "", "title": "", "description": "Magic Fur Brush", "landing-img": ""},
+      {"_id": 7, "name": "", "type": "", "url": "", "title": "", "description": "", "landing-img": ""},
+      {"_id": 8, "name": "", "type": "", "url": "", "title": "", "description": "", "landing-img": ""},
+      {"_id": 9, "name": "", "type": "", "url": "", "title": "", "description": "", "landing-img": ""},
+      {"_id": 10, "name": "", "type": "", "url": "", "title": "", "description": "", "landing-img": ""}
     ]
   }
 
   renderBlocks() {
-    return this.blocks().map(block => {
+    let type;
+    // update variable type with current state
+    // this.state.projetcs.all ? type = 'all' : null;
+    // this.state.projects.ecommerce ? type = 'ecommerce' : null;
+
+    return this.blocks().filter((project) => {
+      return project.type === `${type}`;
+    }).map((project) => {
       return (
-        <Section key={block._id}>
-          <a style={{textDecoration: 'none'}} href={block.url} target="_blank">
+        <Section key={project._id}>
+          <a style={{textDecoration: 'none'}} href={project.url} target="_blank">
             <Block>
-              <h2>{block.name}</h2>
+              <h2>{project.name}</h2>
             </Block>
           </a>
         </Section>
@@ -32,11 +52,17 @@ export default class Projects extends Component {
     })
   }
 
+  componentWillUpdate() {}
+
   componentDidMount() {}
 
   render() {
     return (
       <StyledProjects>
+        <div>
+          <button onClick={() => this.setState({  })}>All</button>
+          <button onClick={() => this.setState({  })}>Ecommerce</button>
+        </div>
         <Container>
           {this.renderBlocks()}
         </Container>
